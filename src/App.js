dc08f38cedaa8b3ca4ca127ca76d3d6473998077
Elena Pavlova <lenlho22@gmail.com>
2019-04-19 15:28:32 +0300

    Added MainLayout

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import MainLayout from './components/MainLayout'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <MainLayout>
+        <div>Hello</div>
+      </MainLayout>
+    )
+  }
+}
+
+export default App
